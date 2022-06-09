26943d8f428c3c55fd20bb3658adcd6b7e6a0130
LukasZaslavskij <zaslavskij@seznam.cz>
2022-06-09 12:30:46 +0200

    Uprava Loginu

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,28 +1,22 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { map, Observable } from "rxjs";
+import { map, Observable, pluck } from "rxjs";
 import { AuthService } from "./auth.service";
 
-@Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate{
-    constructor(private authService: AuthService, private router: Router){}
-    
-  
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot): | boolean
-    | UrlTree
-    | Promise<boolean | UrlTree>
-    | Observable<boolean | UrlTree> {
-        
-    this.authService.autoLogin()
+        | UrlTree
+        | Promise<boolean | UrlTree>
+        | Observable<boolean | UrlTree> {
 
-        if (this.authService.userLoginInfo){
-            console.log(this.authService.userLoginInfo)
-         return true
-        }else
-        console.log(this.authService.userLoginInfo)
-        return this.router.createUrlTree(['/auth'])
-        ;
+        if ('lukas.zaslavskij@cgi.com' === localStorage.getItem("myData")) {
 
+            return true
+        } else
+
+            return this.router.createUrlTree(['/auth']);
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,58 +1,21 @@
 import { computeMsgId } from "@angular/compiler";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
-import { Subject } from "rxjs";
+import { BehaviorSubject, Subject } from "rxjs";
 import { AuthGuard } from "./auth.guard";
 import { User } from "./user.model";
 
-@Injectable({providedIn: 'root'})
-export class AuthService{
-    constructor(private route:Router){}
-    user = new Subject<User>();
-    
-    isUserLogin = false;
-    userLoginInfo=false;
-    hodnota = 'false';
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+    constructor(private route: Router) { }
+    user: BehaviorSubject<any> = new BehaviorSubject<any>({ email: 'dadaa', password: 'bbbbbbbb' });
 
-    login(email: string, password:string){
-        const user = new User(email, password);
-        console.log(user);
-        if(email==='lukas.zaslavskij@cgi.com'){
-                this.isUserLogin=true;
 
-                 
-                
-        }else
-        this.isUserLogin=false;
+    login(email: string, password: string) {
+        this.user.next({ email, password });
 
-        console.log( 'isUserLogin' + this.isUserLogin)
+        localStorage.setItem('myData', this.user.getValue().email)
 
-        const jsonData = JSON.stringify(this.isUserLogin)
-                localStorage.setItem('myData', jsonData)
-                let hodnota= localStorage.getItem("myData")
-                console.log('hodnota je' + hodnota)
-                
-                if (hodnota==='true') {
-                    this.userLoginInfo=true;  
-                }else{
-                    this.userLoginInfo=false;
-                     }
-                console.log('userloginOf v login' + this.userLoginInfo)
-        
-        
-         this.route.navigate(['/skill-list'])
+        this.route.navigate(['/skill-list'])
     }
-    autoLogin (){
-        let hodnota= localStorage.getItem('myData')
-    if (hodnota==='true') {
-        this.userLoginInfo=true;  
-    }else{
-        this.userLoginInfo=false;
-         }
-         console.log('userloginOf v autologin' + this.userLoginInfo)
-    }
-    
-      
-           
-
 }
